9dbf5451b895478d98ef0b914c7cb4cd543c6174
abdisag1 <myabdisa@gmail.com>
2022-01-12 09:37:00 +0000

    modifed quick sort

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,5 +1,6 @@
 #include "sort.h"
 #include <stdio.h>
+
 /**
  * partition - finds the partition for the quicksort using the Lomuto scheme
  * @array: array to sort
@@ -11,34 +12,35 @@
  */
 size_t partition(int *array, ssize_t lo, ssize_t hi, size_t size)
 {
-ssize_t start, end;
-int temp, pivot;
-pivot = array[lo];
-start = lo;
-end = hi;
-while (start < end)
-{
-while (array[start] <= pivot)
-{
-start++;
+  ssize_t i, j;
+  int swap, pivot;
+
+  pivot = array[hi];
+  i = lo - 1;
+  for (j = lo; j < hi; j++)
+    {
+      if (array[j] < pivot)
+	{
+	  i++;
+	  if (i != j)
+	    {
+	      swap = array[i];
+	      array[i] = array[j];
+	      array[j] = swap;
+	      print_array(array, size);
+	    }
+	}
+    }
+  if (array[hi] < array[i + 1])
+    {
+      swap = array[i + 1];
+      array[i + 1] = array[hi];
+      array[hi] = swap;
+      print_array(array, size);
+    }
+  return (i + 1);
 }
-while (array[end] > pivot)
-{
-end--;
-}
-if (start < end)
-{
-temp = array[start];
-array[start] = array[end];
-array[end] = temp;
-}
-}
-temp = array[lo];
-array[lo] = array[end];
-array[end] = temp;
-print_array(array, size);
-return (end);
-}
+
 /**
  * quicksort - sorts a partition of an array of integers
  * @array: array to sort
@@ -50,14 +52,17 @@ return (end);
  */
 void quicksort(int *array, ssize_t lo, ssize_t hi, size_t size)
 {
-ssize_t pivot;
-if (lo < hi)
-{
-pivot = partition(array, lo, hi, size);
-quicksort(array, lo, pivot - 1, size);
-quicksort(array, pivot + 1, hi, size);
+  ssize_t pivot;
+
+  if (lo < hi)
+    {
+      pivot = partition(array, lo, hi, size);
+      quicksort(array, lo, pivot - 1, size);
+      quicksort(array, pivot + 1, hi, size);
+
+    }
 }
-}
+
 /**
  * quick_sort - sorts an array of integers in ascending order using the
  * Quick sort algorithm
@@ -68,7 +73,7 @@ quicksort(array, pivot + 1, hi, size);
  */
 void quick_sort(int *array, size_t size)
 {
-if (array == NULL || size < 2)
-return;
-quicksort(array, 0, size - 1, size);
+  if (array == NULL || size < 2)
+    return;
+  quicksort(array, 0, size - 1, size);
 }
