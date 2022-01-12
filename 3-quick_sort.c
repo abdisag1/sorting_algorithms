2c2b70eb2dfb84eff284d1d1ea0fa3f569515455
abdisag1 <myabdisa@gmail.com>
2022-01-12 06:51:51 +0000

    fixed errors

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -12,35 +12,33 @@
  */
 size_t partition(int *array, ssize_t lo, ssize_t hi, size_t size)
 {
-  ssize_t i, j;
-  int swap, pivot;
-
-  pivot = array[hi];
-  i = lo - 1;
-  for (j = lo; j < hi; j++)
-    {
-      if (array[j] < pivot)
-	{
-	  i++;
-	  if (i != j)
-	    {
-	      swap = array[i];
-	      array[i] = array[j];
-	      array[j] = swap;
-	      print_array(array, size);
-	    }
-	}
-    }
-  if (array[hi] < array[i + 1])
-    {
-      swap = array[i + 1];
-      array[i + 1] = array[hi];
-      array[hi] = swap;
-      print_array(array, size);
-    }
-  return (i + 1);
+ssize_t i, j;
+int swap, pivot;
+pivot = array[hi];
+i = lo - 1;
+for (j = lo; j < hi; j++)
+{
+if (array[j] < pivot)
+{
+i++;
+if (i != j)
+{
+swap = array[i];
+array[i] = array[j];
+array[j] = swap;
+print_array(array, size);
 }
-
+}
+}
+if (array[hi] < array[i + 1])
+{
+swap = array[i + 1];
+array[i + 1] = array[hi];
+array[hi] = swap;
+print_array(array, size);
+}
+return (i + 1);
+}
 /**
  * quicksort - sorts a partition of an array of integers
  * @array: array to sort
@@ -52,17 +50,14 @@ size_t partition(int *array, ssize_t lo, ssize_t hi, size_t size)
  */
 void quicksort(int *array, ssize_t lo, ssize_t hi, size_t size)
 {
-  ssize_t pivot;
-
-  if (lo < hi)
-    {
-      pivot = partition(array, lo, hi, size);
-      quicksort(array, lo, pivot - 1, size);
-      quicksort(array, pivot + 1, hi, size);
-
-    }
+ssize_t pivot;
+if (lo < hi)
+{
+pivot = partition(array, lo, hi, size);
+quicksort(array, lo, pivot - 1, size);
+quicksort(array, pivot + 1, hi, size);
 }
-
+}
 /**
  * quick_sort - sorts an array of integers in ascending order using the
  * Quick sort algorithm
@@ -73,7 +68,7 @@ void quicksort(int *array, ssize_t lo, ssize_t hi, size_t size)
  */
 void quick_sort(int *array, size_t size)
 {
-  if (array == NULL || size < 2)
-    return;
-  quicksort(array, 0, size - 1, size);
+if (array == NULL || size < 2)
+return;
+quicksort(array, 0, size - 1, size);
 }
